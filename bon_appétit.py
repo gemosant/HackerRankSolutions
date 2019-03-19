c4b326ef3e1f1a2cf0e80ca5b15c66d58924ee2d
Rasim <rasim.mutlu@outlook.com>
2019-03-19 18:41:02 +0300

    Add files via upload

---
+++ bon_appétit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def bon_appetit(n, k, cost, b):
+    toplam = 0
+    for i in cost:
+        toplam += i
+    if (b == ((toplam - cost[k]) // 2)):
+        print('Bon Appetit')
+    else:
+        print(cost[k]//2)
+        
+#test_1
+bon_appetit(3, 2, [2, 4, 6], 3)
+
+#test_2
+bon_appetit(3, 2, [2, 4, 6], 6)
+
