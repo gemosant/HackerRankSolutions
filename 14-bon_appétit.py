a3cce11fbd0263c68972d1f88a5c9fcda02daa3d
Rasim <rasim.mutlu@outlook.com>
2019-03-19 18:47:03 +0300

    Update 14-bon_appétit.py

--- 14-bon_appétit.py
+++ 14-bon_appétit.py
@@ -5,7 +5,7 @@ def bon_appetit(n, k, cost, b):
     if (b == ((toplam - cost[k]) // 2)):
         print('Bon Appetit')
     else:
-        print(cost[k]//2)
+        print(b - (cost[k]//2))
         
 #test_1
 bon_appetit(3, 2, [2, 4, 6], 3)
